0c00fefa1248999794ad1138c0619e08e550b155
Yeon Ji Hyung <yhyu0001@student.monash.edu>
2025-08-05 22:27:49 +1000

    efolio week2 task 2.1 activity 1-13 done

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import JSON from './components/JSON.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <!-- <header> -->
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+    </div> -->
+  <!-- </header> -->
 
   <main>
-    <TheWelcome />
+    <JSON />
   </main>
 </template>
 
